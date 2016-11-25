77447a3259b646fedf3699582d5602d212db2e72
Ib Johansen <ib.johansen.jr@gmail.com>
2016-11-26 00:01:35 +0100

    some images and animation

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ class Main extends React.Component {
   render() {
     return (
       <div className="main-container">
+        <div className="background-sky"></div>
+        <div className="background-man"></div>
         <Ad1/>
-        <span className="stars">XXXXXX</span>
       </div>
     );
   }
