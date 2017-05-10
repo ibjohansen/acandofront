0bff0c339a57f47dbcc846996f67f18230c06b8a
Ib Johansen <ib.johansen.jr@gmail.com>
2017-05-10 21:03:41 +0200

    modified app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
 
 app.set('port', (process.env.PORT || 3000));
 
