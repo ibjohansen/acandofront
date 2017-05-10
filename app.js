22c652972a9e75166c9f9f0a14420f1b9ab819bf
Ib Johansen <ib.johansen.jr@gmail.com>
2017-05-10 20:26:27 +0200

    fixed server-bug

--- app.js
+++ app.js
@@ -4,7 +4,5 @@ const app = express();
 app.use('/', express.static(__dirname + '/static', {index: 'index.html'}));
 
 app.listen(3000, () => {
-  initApp(db, store).then(() => {
-    console.log('acando-dreams listening on port 3000!') //eslint-disable-line no-console
-  });
+  console.log('acando-dreams listening on port 3000!') //eslint-disable-line no-console
 });
