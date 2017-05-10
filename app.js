bc5a876c2ce9c516de17694beb65fa93d352e167
Ib Johansen <ib.johansen.jr@gmail.com>
2017-05-10 20:35:45 +0200

    favicon, font, stuff

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static(__dirname + '/static', {index: 'index.html'}));
+app.use(express.static('static'));
 
 app.listen(3000, () => {
-  console.log('acando-dreams listening on port 3000!') //eslint-disable-line no-console
+  console.log('magic-dreams listening on port 3000!') //eslint-disable-line no-console
 });
