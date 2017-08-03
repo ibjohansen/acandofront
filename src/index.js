e1a99634651233f4c277f59c1d07b4a91fe53b09
William W.F.D. Almnes <william.almnes@acando.no>
2017-08-03 15:01:33 +0200

    attempted to work directly with animejs instead of going through react-anime. Created a sample ad

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import AdContainer from './ads/ad-container';
 import ConsoleAd1 from './console-ads/ad1';
 import CityNightAd from './ads/city-night-ad';
 import AnimationAd from './ads/animation-ad';
+import AnimationAd2 from './ads/animation-ad2';
 import LandingPage1 from './landing-page/landing-page-1';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './css/main.scss';
@@ -18,6 +19,7 @@ const Main = () => {
         <Route path="/ad1" component={AdContainer}/>
         <Route path="/city-night-ad" component={CityNightAd}/>
         <Route path="/animation-ad" component={AnimationAd}/>
+        <Route path="/animation-ad2" component={AnimationAd2}/>
         <Route path="/landing-page" component={LandingPage1}/>
       </div>
     </Router>
