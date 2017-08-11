9509c55166d50e6f538ec8f357af906ce612cddc
William Almnes <william.almnes@acando.no>
2017-08-11 10:12:45 +0200

    Needed to fix some more styling issues.

--- src/components/landing-page/landing-page-1.jsx
+++ src/components/landing-page/landing-page-1.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
 
 const LandingPage1 = () => {
   return (
     <div className="landing-page">
+      <Helmet>
+        <body className="is-landing-page" />
+      </Helmet>
       <h1>Jobb i Acando</h1>
       <p>Her skal det stå noe spennende tekst som lokker deg videre til en side på Acando.no?</p>
     </div>
