9d1eb5cce677abc8b053f2fd3184939e34fe1f63
William Almnes <william.almnes@acando.no>
2017-09-12 00:03:05 +0200

    add compression to reduce size on public resources

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '/public')));
 
 const ClientStatsPath = path.join(__dirname, './public/stats.json');
